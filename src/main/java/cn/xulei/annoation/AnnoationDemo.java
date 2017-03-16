1a557d3d6e64830b3d9ca15fe7c71818539e90a1
hyoGirl <594065953@qq.com>
2017-03-16 15:55:45 +0800

    Update AnnoationDemo.java

--- src/main/java/cn/xulei/annoation/AnnoationDemo.java
+++ src/main/java/cn/xulei/annoation/AnnoationDemo.java
@@ -1,5 +1,11 @@
 package cn.xulei.annoation;
 
 public class AnnoationDemo {
-
+  
+    
+    System.out.println("------------------");
+		
+		System.out.println("++++++++++++++++++");
+		
+		System.out.println("=================");
 }
